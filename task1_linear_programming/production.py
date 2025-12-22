49857b91d1a19e11cd5c2d48d854a4e1cb801697
InnaSaienko <inna.saienko.nl@gmail.com>
2025-12-22 15:54:36 +0100

    add docstrings

--- task1_linear_programming/production.py
+++ task1_linear_programming/production.py
@@ -5,14 +5,22 @@ from pulp import LpProblem, LpMaximize, LpVariable
 
 @dataclass
 class Drink:
-   name: str
-   ingredients: dict[str, int]
+    """Represents a drink and ingredients."""
+
+    name: str
+    ingredients: dict[str, int]
+
 
 @dataclass
 class Resources:
+    """Holds available resources limits."""
+
     limits: dict[str, int]
 
+
 class ProductionModel:
+    """Solve production optimization problem."""
+
     def __init__(self, drinks: list[Drink], resources: Resources):
         self.model = LpProblem("Production", LpMaximize)
         self.vars = {drink.name: LpVariable(drink.name, lowBound=0, cat="Integer") for drink in drinks}
