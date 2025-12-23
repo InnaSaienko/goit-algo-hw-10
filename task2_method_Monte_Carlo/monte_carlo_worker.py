91d7159e93906c2ae44980e430fcc23184ff2b03
InnaSaienko <inna.saienko.nl@gmail.com>
2025-12-23 11:52:07 +0100

    definite integral using Monte Carlo method

--- task2_method_Monte_Carlo/monte_carlo_worker.py
+++ task2_method_Monte_Carlo/monte_carlo_worker.py
@@ -1,3 +1,11 @@
-def monte_carlo_worker(func, a, b):
+import random
+
+from typing import Callable
+
+
+def monte_carlo_worker(function: Callable[[float], float], a: float, b: float, n: int) -> float:
     """Estimate integral using Monte Carlo method."""
-    pass
+
+    values = [function(random.uniform(a, b)) for _ in range(n)] # list random points between a and b
+    estimate = (b - a) * sum(values) / n # average integral approximation
+    return estimate
