4685f2b11cedf452487ce74d322020a596697469
InnaSaienko <inna.saienko.nl@gmail.com>
2025-12-23 15:25:52 +0100

    pass to visualization number of points

--- task2_method_Monte_Carlo/main.py
+++ task2_method_Monte_Carlo/main.py
@@ -14,16 +14,16 @@ def main() -> None:
     """Run Monte Carlo integration"""
 
     a, b = 0, 2
-    number_random_points = 100000
+    number_of_points = 100000
 
-    result_monte_carlo = monte_carlo_worker(integration_function, a, b, number_random_points)
+    result_monte_carlo = monte_carlo_worker(integration_function, a, b, number_of_points)
     result_quad = exact_integral(integration_function, a, b)
 
     print("Monte Carlo integration: ", result_monte_carlo)
     print("Exact:", result_quad)
     print(f"Absolute deviation: {abs(result_monte_carlo - result_quad)}")
 
-    visualizing_result(integration_function, a, b)
+    visualizing_result(integration_function, a, b, number_of_points)
 
 
 if __name__ == "__main__":
